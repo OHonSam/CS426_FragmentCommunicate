1ea89a0b5dc2494c128e62e5f44a862ccdedacf1
OHonSam <ohsam22@apcs.fitus.edu.vn>
2024-06-25 18:54:12 +0700

    Add more colors, dimens, styles to bring feedback bar from FragmentCommunicate into this app

---
+++ app/src/main/java/com/example/android/songdetail/FeedbackFragment.java
@@ -0,0 +1,112 @@
+package com.example.android.songdetail;
+
+import android.content.Context;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass that shows a question
+ * with radio buttons for providing feedback. If the user
+ * clicks "Yes" the text header changes to "Article: Like".
+ * If the user clicks "No" the text header changes to "Thanks".
+ */
+public class FeedbackFragment extends Fragment {
+
+    // The radio button choice has 3 states: 0 = yes, 1 = no,
+    // 2 = default (no choice). Using only 0 and 1.
+    private static final int YES = 0;
+    private static final int NO = 1;
+    private static final int NONE = 2;
+    public int mRadioButtonChoice = NONE;
+    OnFragmentInteractionListener mListener;
+    private static final String CHOICE = "choice";
+
+    public FeedbackFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Creates the view for the fragment.
+     *
+     * @param inflater           LayoutInflater to inflate any views in the fragment
+     * @param container          ViewGroup of parent view to attach fragment
+     * @param savedInstanceState Bundle for previous state
+     * @return rootView
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment.
+        final View rootView = inflater.inflate(R.layout.fragment_feedback,
+                container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+        // Retrieve the radio button choice (if a choice was made), and pre-select the radio button.
+        if (getArguments().containsKey(CHOICE)) {
+            // A choice was made, so get the choice.
+            mRadioButtonChoice = getArguments().getInt(CHOICE);
+            // Check the radio button choice.
+            if (mRadioButtonChoice != NONE) {
+                radioGroup.check
+                        (radioGroup.getChildAt(mRadioButtonChoice).getId());
+            }
+        }
+        // Set the radioGroup onCheckedChanged listener.
+        radioGroup.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        View radioButton = radioGroup.findViewById(checkedId);
+                        int index = radioGroup.indexOfChild(radioButton);
+                        TextView textView =
+                                rootView.findViewById(R.id.fragment_header);
+                        switch (index) {
+                            case YES: // User chose "Yes".
+                                textView.setText(R.string.yes_message);
+                                mRadioButtonChoice = YES;
+                                mListener.onRadioButtonChoice(YES);
+                                break;
+                            case NO: // User chose "No".
+                                textView.setText(R.string.no_message);
+                                mRadioButtonChoice = NO;
+                                mListener.onRadioButtonChoice(NO);
+                                break;
+                            default: // No choice made.
+                                mRadioButtonChoice = NONE;
+                                mListener.onRadioButtonChoice(NONE);
+                                break;
+                        }
+                    }
+                });
+
+        // Return the View for the fragment's UI.
+        return rootView;
+    }
+
+    public static FeedbackFragment newInstance(int choice) {
+        FeedbackFragment fragment = new FeedbackFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(CHOICE, choice);
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
+    interface OnFragmentInteractionListener {
+        void onRadioButtonChoice(int choice);
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new ClassCastException(context.toString()
+                    + getResources().getString(R.string.exception_message));
+        }
+    }
+}
